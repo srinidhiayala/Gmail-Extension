9f5c9df035f7a144421f2204f2f8cdf45ffef650
srinidhiayala <145507362+srinidhiayala@users.noreply.github.com>
2024-07-19 16:56:13 -0400

    pop up button but not working

--- popup-script.js
+++ popup-script.js
@@ -1,15 +1,25 @@
 const recipient = document.querySelector("#recipient");
 const subject = document.querySelector("#subject");
 const body_message = document.querySelector("#message");
+const popup = document.getElementById('popup');
+const closePopup = document.getElementById('closePopup');
+  
 
 document.querySelector("#submit").addEventListener("click", function() {
     
     chrome.runtime.sendMessage({message: "send_email", data: {recipient:recipient.value,subject:subject.value,body_message:body_message.value}});
+    popup.classList.remove('popup-hidden');
+    popup.classList.add('popup-visible');
   });
 
 document.querySelector("#draft").addEventListener("click", function() {
     chrome.runtime.sendMessage({message: "draft_email"});
   });
+
+closePopup.addEventListener('click', function() {
+    popup.classList.remove('popup-visible');
+    popup.classList.add('popup-hidden');
+});
   
 
 
